5bbda38011b07e4b8eacf7d478b9af0bc58c3093
smfreitas <samfreitas687@hotmail.com>
2022-12-06 21:54:48 -0800

    Updates

---
+++ Novel-Project-V2/src/main/java/com/novel/NovelProjectV2Application.java
@@ -0,0 +1,13 @@
+package com.novel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelProjectV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelProjectV2Application.class, args);
+	}
+
+}
