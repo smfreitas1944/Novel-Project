02faef859e96c8bb908b15aceece41280c9859fc
smfreitas <samfreitas687@hotmail.com>
2022-12-06 22:22:03 -0800

    Novel model update

--- Novel-Project-V2/src/main/java/com/novel/model/Novel.java
+++ Novel-Project-V2/src/main/java/com/novel/model/Novel.java
@@ -1,5 +1,37 @@
 package com.novel.model;
 
-public class Novel {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "novel")
+public class Novel {
+	@Id  //Used to define the primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  //used to define the primary key generation strategy
+	@Column(name = "novel_id") //annotation is used to define the properties of the column that will be mapped to the annotated field. You can define several properties like name, length, nullable, updateable, etc.
+	private int NovelID;
+	
+	@Column(name = "novel_name")
+	private String NovelName;
+	
+	@Column(name = "novel_author")
+	private String NovelAuthor;
+	
+	@Column(name = "novel_genre")
+	private String NovelGenre;
+	
+	public Novel(String NovelName, String NovelAuthor, String NovelGenre) {
+			this.NovelName = NovelName;
+			this.NovelAuthor = NovelAuthor;
+			this.NovelGenre = NovelGenre;
+	}
+	
+	
+	
 }
--- Novel-Project/src/main/java/com/novel/models/Novel.java
+++ Novel-Project/src/main/java/com/novel/models/Novel.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Data
 @Entity
-@Table(name = "NOVEL")
+@Table(name = "novel")
 public class Novel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
